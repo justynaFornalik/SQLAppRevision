1b4490781e3065998b9f29dc757beeeb553d1683
Justyna Fornalik <jfornalik@gmail.com>
2018-02-14 16:53:02 +0100

    Create Applicant class

---
+++ src/model/Applicant.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Applicant extends Person {
+    private int applicationCode;
+
+    public Applicant(String firstName, String lastName) {
+        super(firstName, lastName);
+    }
+
+    public Applicant(String firstName, String lastName, String phoneNumber, String email, int applicationCode) {
+        super(firstName, lastName, phoneNumber, email);
+        this.applicationCode = applicationCode;
+    }
+
+    public int getApplicationCode() {
+        return applicationCode;
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + ", " + applicationCode;
+    }
+}
--- src/model/Mentor.java
+++ src/model/Mentor.java
@@ -1,7 +1,5 @@
 package model;
 
-import dao.MentorDao;
-
 public class Mentor extends Person{
 
     String nickName;
