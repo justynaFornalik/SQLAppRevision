9a3ae8adac16f33dfef66d42ac21bb37dbb84967
Justyna Fornalik <jfornalik@gmail.com>
2018-02-14 14:27:26 +0100

    Create methods to get connection inside DbConnection class

--- src/dao/DbConnection.java
+++ src/dao/DbConnection.java
@@ -1,5 +1,33 @@
 package dao;
 
-public class DbConnection {
+import java.sql.*;
+
+public final class DbConnection {
+
+    private static DbConnection instance = null;
+    private Connection connection = null;
+    private final String DBNAME = "jdbc:sqlite:db.db";
+
+    private static DbConnection getInstance(){
+        if(instance == null){
+            instance = new DbConnection();
+        }
+        return instance;
+    }
+
+    private void getConnectionToDb(){
+        try{
+            Class.forName("org.sqlite.JDBC");
+            this.connection = DriverManager.getConnection(DBNAME);
+        } catch (SQLException | NullPointerException e){
+            System.out.println("Database connection problem");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Class not found");
+        }
+    }
+
+    public Connection getConnection(){
+        return this.connection;
+    }
 
 }
